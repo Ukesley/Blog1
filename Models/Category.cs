abba512361b5dd3cdc238bedf517f86548f01737
kesley <kesleynbarros@gmail.com>
2025-03-23 00:50:06 -0300

    Update database

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,8 +6,8 @@ namespace Blog.Models
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Slug { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
         [Write(false)]
         public List<Post> Posts { get; set; }
     }
--- obj/Debug/net9.0/Blog1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Blog1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb841180025f61d5c5ba0d93291424d643446b1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae2032a918f4890a38313a953d104b626ca0aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
